ec5d79aaef08458b1c2c9db980221614244ff76b
Jerome Boulanger <jeromeb@mrc-lmb.cam.ac.uk>
2018-10-02 11:27:20 +0100

    improve compatibility with octave

--- display_sim_parameter.m
+++ display_sim_parameter.m
@@ -7,17 +7,16 @@ function display_sim_parameter(p,pixel_size,wavelength,numerical_aperture)
 %
 % Jerome Boulanger 2018
 
-fprintf('period\tangle\tampl.(%)\tshift(%)\n');
+fprintf(1,'period\tangle\tampl.(%%)\tshift(%%)\n');
 for l = 1:numel(p)
-    fprintf('%.3f\t%.2f\t%.3f\t%.2f\n', p(l).period, p(l).orientation,...
+    fprintf(1,'%.3f\t%.2f\t%.3f\t%.2f\n', p(l).period, p(l).orientation,...
         p(l).amplitude*100,p(l).shift*100);
 end
 
 if nargin > 1
     r0 = wavelength / (2*numerical_aperture);
     p0 = mean([p(:).period]) * pixel_size;
-    fprintf('period %dnm -> %d%% cutoff\n',  round(p0), round(100*r0 / p0));
+    fprintf(1,'period %dnm -> %d%% cutoff\n',  round(p0), round(100*r0 / p0));
 end
 
- fprintf('amplitude avg : %f%%\n',  mean([p(:).amplitude])*100);
- 
+ fprintf(1,'amplitude avg : %.2f%%\n',  mean([p(:).amplitude])*100);
--- simulation.m
+++ simulation.m
@@ -5,7 +5,7 @@
 
 clear all
 filename = 'periodic_pattern.tif';
-period = 12; % modulation period (before zoom)
+period = 4.75; % modulation period (before zoom)
 cutoff = 4; % cutoff frequency
 zoom = 2; % zoom factor (1 or 2)
 noise_std = 2; % Gaussin noise level
@@ -17,20 +17,23 @@ m = generate_sim_modulation(size(obj), p); % generate a set of modulation
 otf = generate_otf(size(obj,1), cutoff); % compute an OTF
 [data,otf] = generate_sim_data(obj,m,otf,zoom,noise_std); % generate SIM data
 tic;
-phat = estimate_sim_parameters(data,otf); % estimate SIM parameters 
+phat = estimate_sim_parameters(data,otf); % estimate SIM parameters
 display_sim_parameter(phat); % display a summary of the parameters
 mhat = generate_sim_modulation(size(obj), phat); % and compute the modulation
 [im,sw] = reconstruct_sim_base(data,phat,otf,zoom,wiener_parameter,mask_amplitude);
 toc;
-% display results
-figure(1), clf; 
-subplot(121), imshow(im,[]);
-subplot(122), fftshow(im);
-% compute the root mean square error 
+% compute the root mean square error
 c = polyfit(obj(:),im(:),1);
 im = (im - c(2)) / c(1);
-avg = imresize(mean(data,3), size(obj));
+avg = mean(data,3);
+avg = real(ifft2(ifftshift(padarray(fftshift(fft2(avg)),size(avg)/2))));
 c = polyfit(obj(:),avg(:),1);
 avg = (avg - c(2)) / c(1);
 fprintf(1,'Error AVG RMS:%f\n', sqrt(mean((avg(:)-obj(:)).^2))/mean(obj(:)));
 fprintf(1,'Error SIM RMS:%f\n', sqrt(mean((im(:)-obj(:)).^2))/mean(obj(:)));
+% display results
+figure(1), clf;
+subplot(221), imshow(avg,[]);
+subplot(223), fftshow(avg,otf);
+subplot(222), imshow(im,[]);
+subplot(224), fftshow(im,otf);
